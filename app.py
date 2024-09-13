6cba5b968d3634a14c93ae9403474b105bcddddc
Benjaminu10 <benjaminu10@nycstudents.net>
2024-09-13 10:12:20 -0400

    app.py
    9/13/24

--- app.py
+++ app.py
@@ -8,24 +8,33 @@
 # else:
 #     print('cold')
 
-number =2
-if (number %2) == 0:
-     print('Even')
+# number=3
+# if (number %2) == 0:
+#      print('Even')
+# else:
+#      print('Odd')
+
+
+# bill=100
+# Service = input("How was the service? ")
+# if Service == "bad":
+#     print(bill)
+# if Service == "okay":
+#     bill =+ 1.15*bill
+#     print(bill)
+# if Service == "good":
+
+#     bill =+ 1.20*bill
+#     print(bill)
+# if Service == "great":
+#     bill =+ 1.25*bill
+#     print(bill)
+
+number = float(input("Insert a number. "))
+
+for i in range(1-number)
+if (number %5) == 0:
+    print("yes")
 else:
-     print('Odd')
-bill=100
+    print("no")
 
-
-
-Service = input("How was the service? ")
-if Service == "bad":
-    print(bill)
-if Service == "okay":
-    bill =+ 1.15*bill
-    print(bill)
-if Service == "good":
-    bill =+ 1.20*bill
-    print(bill)
-if Service == "great":
-    bill =+ 1.25*bill
-    print(bill)
