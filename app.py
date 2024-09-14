170f72f89af215991fbc28d298d014072e3ecf63
Benjaminu10 <benjaminu10@nycstudents.net>
2024-09-13 22:33:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,11 @@
 # else:
 #     print('cold')
 
-<<<<<<< HEAD
-# number=7
-=======
 # number=3
->>>>>>> 6cba5b968d3634a14c93ae9403474b105bcddddc
 # if (number %2) == 0:
 #      print('Even')
 # else:
 #      print('Odd')
-<<<<<<< HEAD
-
 
 
 # bill=100
@@ -45,30 +39,3 @@ def find_factors(x):
 number = int(input("Insert a number. "))
 factors = find_factors(number)
 print(factors)
-=======
-
-
-# bill=100
-# Service = input("How was the service? ")
-# if Service == "bad":
-#     print(bill)
-# if Service == "okay":
-#     bill =+ 1.15*bill
-#     print(bill)
-# if Service == "good":
-
-#     bill =+ 1.20*bill
-#     print(bill)
-# if Service == "great":
-#     bill =+ 1.25*bill
-#     print(bill)
-
-number = float(input("Insert a number. "))
-
-for i in range(1-number)
-if (number %5) == 0:
-    print("yes")
-else:
-    print("no")
-
->>>>>>> 6cba5b968d3634a14c93ae9403474b105bcddddc
